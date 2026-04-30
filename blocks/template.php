5d7d7b6a9a84fc7719022cba3303d1b4adc03185
BLP Development <dev@blpboard.local>
2026-04-30 16:52:42 +0300

    feat: replace favicon with BLITE logo from blite-light.ru
    - Downloaded favicon.ico from https://blite-light.ru/
    - Removed old custom green favicon.svg
    - Updated template.php to reference favicon.ico

--- blocks/template.php
+++ blocks/template.php
@@ -59,7 +59,7 @@ $ga4_id        = 'G-PLACEHOLDER20260420';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/svg+xml" href="/images-convert/favicon.svg">
+    <link rel="icon" type="image/x-icon" href="/images-convert/favicon.ico">
     <title><?php echo htmlspecialchars($page_title, ENT_QUOTES, 'UTF-8'); ?></title>
     <?php if ($page_desc): ?>
     <meta name="description" content="<?php echo htmlspecialchars($page_desc, ENT_QUOTES, 'UTF-8'); ?>">
